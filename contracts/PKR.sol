44611826b6c3baf43cd16c3d36c05a0df03e4e64
ABDul Rehman <devabdee@gmail.com>
2022-11-21 13:21:57 +0500

    PKR Token added

---
+++ contracts/PKR.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PKR is ERC20 {
+
+    uint256 public supplyMinted;
+
+    uint256 public constant MAX_SUPPLY = 50000 * 10**18;
+    
+    constructor(uint initialSupply) ERC20("Digital PKR", "PKR") {
+        supplyMinted += initialSupply * 10**18;
+        _mint(msg.sender, initialSupply * 10**18);
+    }
+
+    function mintPKR(uint256 inputAmount) public {
+        require(MAX_SUPPLY > supplyMinted + inputAmount * 10**18, "All PKR minted");
+        supplyMinted += inputAmount * 10**18;
+        _mint(msg.sender, inputAmount * 10**18);
+    }
+
+    ///// 0xFC0966D72e05dA834d33E8B36ac64C4878d374B9
+    
+}
