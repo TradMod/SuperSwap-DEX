fc6eebd4ff41ee7bc8894f1017cab8683c195037
ABDul Rehman <devabdee@gmail.com>
2022-11-23 16:55:33 +0500

    navich

--- contracts/SuperSwap.sol
+++ contracts/SuperSwap.sol
@@ -4,22 +4,55 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+    /**
+     * @author ABDul Rehman
+     * @title  SuperSwap V1
+    */
 contract SuperSwap is ERC20 {
 
     IERC20 public PKR;
+
+    uint256 public constant SWAP_FEE = 1;
 
     constructor(address addressPKR) ERC20("Super Swap", "SUPER"){
         PKR = IERC20(addressPKR);
     }
 
+    /**
+     * @notice rPKR = PKR reserves
+     * @notice rETH = ETH reserves
+    */
     function addLiquidity(uint256 amountPKR) public payable {
-        PKR.transferFrom(msg.sender, address(this), amountPKR);
+        if(reservePKR() == 0){
+            PKR.transferFrom(msg.sender, address(this), amountPKR);
+            uint256 LPtokens = address(this).balance;
+            _mint(msg.sender, LPtokens);
+        } else {
+            uint256 rPKR = reservePKR();
+            uint256 rETH = address(this).balance - msg.value;
+            uint256 amount_PKR = (msg.value * rPKR) / rETH;
+            require(amountPKR >= amount_PKR, "not enough PKR sent");
+            PKR.transferFrom(msg.sender, address(this), amount_PKR);
+            uint256 LPtokens = (totalSupply() * msg.value) / rETH;
+            _mint(msg.sender, LPtokens);
+        }
+    }
+
+    function removeLiquidity(uint256 amount) public  {
+        require(amount > 0, "invalid amount");
+        uint256 ethAmount = (address(this).balance * amount) / totalSupply();
+        uint256 tokenAmount = (reservePKR() * amount) / totalSupply();
+        _burn(msg.sender, amount);
+        (bool pass, ) = payable(msg.sender).call{value: ethAmount}("");
+        require(pass);
+        PKR.transfer(msg.sender, tokenAmount);
     }
 
     function swapETHtoPKR() public payable {
         require(msg.value > 0, "Not enough ETH sent");
         uint256 rPKR = reservePKR();
-        uint256 amountPKR = getAmount(msg.value, address(this).balance - msg.value, rPKR);
+        uint256 rETH = address(this).balance - msg.value;
+        uint256 amountPKR = getAmount(msg.value, rETH, rPKR);
         PKR.transfer(msg.sender, amountPKR);
     }
 
@@ -33,8 +66,10 @@ contract SuperSwap is ERC20 {
     }
 
     function getAmount(uint256 inputAmount, uint256 reserveX, uint256 reserveY) public pure returns(uint256 outputAmount) {
-        uint256 numerator = (reserveY * inputAmount);
-        uint256 denominator = (reserveX + inputAmount);
+        uint256 withFee = 100 - SWAP_FEE; 
+        uint256 inputAmountWithFee = inputAmount * withFee;
+        uint256 numerator = (reserveY * inputAmountWithFee);
+        uint256 denominator = (reserveX + inputAmountWithFee);
         return numerator / denominator;
     }
 
