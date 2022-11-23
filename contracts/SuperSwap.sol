4f5ebedd92af5ec2cee1ce11939db945075aa48b
ABDul Rehman <devabdee@gmail.com>
2022-11-23 10:46:24 +0500

    helper functions added

--- contracts/SuperSwap.sol
+++ contracts/SuperSwap.sol
@@ -18,18 +18,30 @@ contract SuperSwap is ERC20 {
 
     function swapETHtoPKR() public payable {
         require(msg.value > 0, "Not enough ETH sent");
-        uint256 rPKR = reservePKR();
-        uint256 amountPKR = (rPKR * msg.value) / (address(this).balance + msg.value);
+        uint256 amountPKR = getPKR(msg.value);
         PKR.transfer(msg.sender, amountPKR);
     }
 
     function swapPKRtoETH(uint256 amountPKR) public {
         require(amountPKR > 0, "Not enough PKR sent");
         PKR.transferFrom(msg.sender, address(this), amountPKR);
-        uint256 rPKR = reservePKR();
-        uint256 amountETH = (address(this).balance * amountPKR) / (rPKR + amountPKR);
+        uint256 amountETH = getETH(amountPKR);
         (bool success, ) = payable(msg.sender).call{value: amountETH}("");
         require(success);
+    }
+
+    function getAmount(uint256 inputAmount, uint256 reserveX, uint256 reserveY) public pure returns(uint256 outputAmount) {
+        return (reserveY * inputAmount) / (reserveX + inputAmount);
+    }
+
+    function getETH(uint256 inputAmountPKR) public view returns(uint256 outputAmountETH) {
+        uint256 rPKR = reservePKR();
+        return getAmount(inputAmountPKR, address(this).balance, rPKR);
+    }
+
+    function getPKR(uint256 inputAmountETH) public view returns(uint256 outputAmountPKR) {
+        uint256 rPKR = reservePKR();
+        return getAmount(inputAmountETH, rPKR, address(this).balance);
     }
 
     function reservePKR() public view returns(uint256){
